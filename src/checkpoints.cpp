4b0ff0191eed5d98285f43ac17446fafd26ff848
DarkCredit <support@darkcredit.net>
2015-02-10 18:25:33 -0500

    .

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -29,6 +29,7 @@ namespace Checkpoints
         ( 150, uint256("0x00000000013c5dea0fab41e4bcec477d1a4cfdcc7131f9b2b7b9fd9c18a12089"))
         ( 200, uint256("0x00000000067dcba999d6cdcbaf92e76ae699455a8fbca329a8907c07fa9c5805"))
         ( 250, uint256("0x0000000005aeb9c009ecea2bb2d17086215a934636e2c934313ec45ef55f04a0"))
+        ( 1400, uint256("0x00000000021d00cc636b96b517c5bd2124eef7851761f4e15b759816df2ab161"))
     ;
 
     // TestNet has no checkpoints
