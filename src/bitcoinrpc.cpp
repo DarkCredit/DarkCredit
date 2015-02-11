dc8247f8ff967fe78feed3353d0602f66c52b1cb
DarkCredit <support@darkcredit.net>
2015-02-11 06:30:53 -0500

    Revert "rpc port fix"
    This reverts commit dcfe246d64c188ec4551de22531f52a372df0f06.

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 8812 : 8810;
+    return GetBoolArg("-testnet", false) ? 51991 : 51990;
 }
 
 Object JSONRPCError(int code, const string& message)
